f026d68abb35eca93f7a1084f8f39b4e7f5c8425
tongnamuu <thban33@naver.com>
2022-06-22 01:11:13 +0900

    test css modules

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,11 @@
 import Link from "next/link"
 import { useRouter } from "next/router"
+import styles from "./NavBar.module.css"
 
 export default function NavBar() {
     const router = useRouter()
 
-    return <nav>
+    return <nav className={styles.nav}>
         <Link href="/">
             <a style={{ color: router.pathname === "/" ? "purple": "green" }}>Home</a>
         </Link>
